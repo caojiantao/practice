dfe96968766b7419946cd1d90f0be9d41b1948f3
caojiantao <caojiantao30@gmail.com>
2018-05-09 16:39:21 +0800

    线程切换

---
+++ src/main/java/com/cjt/practice/exam/ThreadSwitch.java
@@ -0,0 +1,65 @@
+package com.cjt.practice.exam;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author caojiantao
+ */
+public class ThreadSwitch {
+
+    public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger();
+        new Thread(() -> {
+            while (true) {
+                synchronized (count) {
+                    try {
+                        while (count.intValue() % 10 != 0) {
+                            count.wait();
+                        }
+                        System.out.println("AAA");
+                        TimeUnit.SECONDS.sleep(1);
+                        count.incrementAndGet();
+                        count.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                synchronized (count) {
+                    try {
+                        while (count.intValue() % 10 != 1) {
+                            count.wait();
+                        }
+                        System.out.println("BBB");
+                        TimeUnit.SECONDS.sleep(1);
+                        count.incrementAndGet();
+                        count.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                synchronized (count) {
+                    try {
+                        while (count.intValue() % 10 != 2) {
+                            count.wait();
+                        }
+                        System.out.println("CCC");
+                        TimeUnit.SECONDS.sleep(1);
+                        count.incrementAndGet();
+                        count.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
